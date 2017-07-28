1f0a063e569bf859b006737fbef4f42f81b1e525
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-07-28 13:09:19 -0500

    added comments

--- events.js
+++ events.js
@@ -1,8 +1,11 @@
 console.log("events.js, yo!");
 
+//variables at the top getting the HTML elements
 let output = document.getElementById("output-target");
 let section = document.getElementsByTagName("section");
 
+
+//one of the objectives of the activity was to style a few of the elements using Javascript and not CSS. This was the way I accessed those specific elements and did it!
 document.getElementsByTagName("section")[0].style.fontWeight = "bold";
 document.getElementsByTagName("section")[5].style.fontWeight = "bold";
 document.getElementsByTagName("section")[5].style.fontStyle = "italic";
@@ -10,28 +13,32 @@ document.getElementsByTagName("section")[5].style.fontStyle = "italic";
 
 //TARGET SECTIONS TO PRINT TO OUTPUT WHEN CLICKED
 		function clickedSection(click){
-			// console.log("click", click);
-			//this is taking the inner HTML and making element text
+			//this is taking the inner HTML of the element that I clicked and making element text for...
 			let sectionText = click.target.innerHTML;
+
+			//this output location, which is linked up in the variables at the top
 			output.innerHTML = `You clicked on the ${sectionText} section`
 		};
 
+		//uh oh....here is a for loop! so, since the sections are an HTML collection, I have to access them in a slightly different way.
 		for (let i = 0; i < section.length; i++){
 			//this gets .item(i) because it is NOT an array. It is an HTML collection
 			section.item(i).addEventListener("click", clickedSection);
 		};
 
 //CHANGE TEXT IN OUTPUT ON H1 MOUSE OVER AND MOUSE OUT
-		//if I did not specify which h1 tag, I would have had to do another for loop or forEach.
+		//if I did not specify which h1 tag, I would have had to do another for loop or forEach. How did I specify? You see that [0]? ahhh...you get it! That is the index of the element I am trying to get.
 		let header = document.getElementsByTagName("h1")[0];
 
+			//now, I am adding event listenters to that element
 			header.addEventListener("mouseover", headerMouseOver);
 			header.addEventListener("mouseout", headerMouseOut);
 
+			//when you mouseOVER, you get this message
 		function headerMouseOver(event){
 			output.innerHTML = `You moved your mouse over the header`;
 		};
-
+			//when you mouseOUT, you get this message
 		function headerMouseOut(event){
 			output.innerHTML = `You left me!!`;
 		}
@@ -47,15 +54,18 @@ document.getElementsByTagName("section")[5].style.fontStyle = "italic";
 
 //When you click the "Add color" button, the guinea-pig element's text color should change to blue.
 let guineaPig = document.getElementById("guinea-pig");
-
+//get the ID
 document.getElementById("add-color")
+	//add event listener
 	.addEventListener("click", function(){
+		//make the class toggle back and forth on click
 		guineaPig.classList.toggle("add-color")
 
 	});
 
-//When you click the "Hulkify" button, the guinea-pig element's font size should become much larger.
+	//same idea as above:
 
+//When you click the "Hulkify" button, the guinea-pig element's font size should become much larger.
 document.getElementById("make-large")
 	.addEventListener("click", function(){
 		guineaPig.classList.toggle("hulkify")
@@ -75,6 +85,8 @@ document.getElementById("add-rounding")
 		guineaPig.classList.toggle("add-rounding")
 
 	});
+
+	//coffee and wine? please!!!
 
 
 
